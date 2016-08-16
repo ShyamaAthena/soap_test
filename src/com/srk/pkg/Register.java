f62dc135fdf40d634d24b852fd2af735e905dac9
Shyama MV <mshyama@corp.athenahealth.com>
2016-08-16 15:52:32 +0530

    First commit

---
+++ src/com/srk/pkg/Register.java
@@ -0,0 +1,32 @@
+package com.srk.pkg;
+
+import java.io.*;  
+import java.sql.*;  
+import javax.servlet.ServletException;  
+import javax.servlet.http.*;  
+
+
+  
+public class Register extends HttpServlet {  
+public void doPost(HttpServletRequest request, HttpServletResponse response)  
+            throws ServletException, IOException {  
+  
+response.setContentType("text/html");  
+PrintWriter out = response.getWriter();  
+          
+String n=request.getParameter("userName");  
+String p=request.getParameter("userPass");  
+String e=request.getParameter("userEmail");  
+String c=request.getParameter("userCountry");  
+          
+try{  
+
+out.print("You are successfully registered...");  
+      
+          
+}catch (Exception e2) {System.out.println(e2);}  
+          
+out.close();  
+}  
+  
+}  
